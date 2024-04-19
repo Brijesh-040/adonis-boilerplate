34535514d5f08302a6b4761b2425d30a1bd048dc
Brijesh Lakhani <brijesh.lakhani@techivies.com>
2024-04-19 11:05:32 +0530

    update migration

---
+++ database/migrations/1713271284352_staff.ts
@@ -0,0 +1,28 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'staff'
+
+  public async up () {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+
+      table.string("name");
+      table.string("role");
+      table.string("address");
+      table.string("city");
+      table.string("state");
+      table.string("country");
+      table.integer("salary");
+      /**
+       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
+       */
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down () {
+    this.schema.dropTable(this.tableName)
+  }
+}
